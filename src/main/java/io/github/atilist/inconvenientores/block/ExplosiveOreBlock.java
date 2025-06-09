bbafaea662c0ea7c0793aa039e414781d74cc98f
Atilist <71230342+Atilist@users.noreply.github.com>
2025-06-09 19:39:29 +0200

    Improve Explosive Ore Behaviour More Explosions are created when it blows up, leading to Chain Reactions. Blocks no longer drop themselves to prevent any chance of getting free TNT.

--- src/main/java/io/github/atilist/inconvenientores/block/ExplosiveOreBlock.java
+++ src/main/java/io/github/atilist/inconvenientores/block/ExplosiveOreBlock.java
@@ -6,6 +6,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.util.Identifier;
 
+import java.util.Random;
+
 public class ExplosiveOreBlock extends LazyBlockTemplate {
     public ExplosiveOreBlock(Identifier identifier, Material material) {
         super(identifier, material, 1.5F, Block.STONE_SOUND_GROUP);
@@ -14,5 +16,15 @@ public class ExplosiveOreBlock extends LazyBlockTemplate {
     @Override
     public void afterBreak(World world, PlayerEntity playerEntity, int x, int y, int z, int meta) {
         world.createExplosion(null, x + 0.5, y + 0.5, z + 0.5, 2.5F);
+    }
+
+    @Override
+    public void onDestroyedByExplosion(World world, int x, int y, int z) {
+        world.createExplosion(null, x + 0.5, y + 0.5, z + 0.5, 2.5F);
+    }
+
+    @Override
+    public int getDroppedItemCount(Random random) {
+        return 0;
     }
 }
